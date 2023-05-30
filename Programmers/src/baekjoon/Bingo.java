8418eef6766739cae0a7db8e1057c4c05b565dc0
dmstjd0214@naver.com <dmstjd0214@naver.com>
2023-05-30 15:09:43 +0900

    소가 가는길 , 빙고 문제 해결

---
+++ Programmers/src/baekjoon/Bingo.java
@@ -0,0 +1,78 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Bingo {
+    static int[][] bingoMap;
+    static int line;
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        bingoMap = new int[5][5];
+        line = 0;
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                bingoMap[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int a = 1; a <= 25; a++) {
+            int bingoNum = sc.nextInt();
+            for (int i = 0; i < 5; i++) {
+                for (int j = 0; j < 5; j++) {
+                    if (bingoMap[i][j] == bingoNum) bingoMap[i][j] = 0;
+                }
+            }
+            rightStraight();
+            downStraight();
+            rightCross();
+            leftCross();
+
+            if (line >= 3) {
+                System.out.println(a);
+                break;
+            }
+            line = 0;
+        }
+
+    }
+
+    public static void rightStraight() {
+
+        for (int i = 0; i < 5; i++) {
+            int bingoLine = 0;
+            for (int j = 0; j < 5; j++) {
+                if (bingoMap[i][j] == 0) bingoLine++;
+            }
+            if (bingoLine == 5) line++;
+        }
+    }
+
+    public static void downStraight() {
+        for (int i = 0; i < 5; i++) {
+            int bingoLine = 0;
+            for (int j = 0; j < 5; j++) {
+                if (bingoMap[j][i] == 0) bingoLine++;
+            }
+            if (bingoLine == 5) line++;
+        }
+    }
+
+    public static void rightCross() {
+        int bingoLine = 0;
+        for (int i = 0; i < 5; i++) {
+            if (bingoMap[i][i] == 0) bingoLine++;
+        }
+        if (bingoLine == 5) line++;
+    }
+
+    public static void leftCross() {
+        int bingoLine = 0;
+        for (int i = 0; i < 5; i++) {
+            if (bingoMap[i][4 - i] == 0) bingoLine++;
+        }
+        if (bingoLine == 5) line++;
+    }
+}
--- Programmers/src/baekjoon/CowWhyRunWay1.java
+++ Programmers/src/baekjoon/CowWhyRunWay1.java
@@ -3,18 +3,30 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class CowWhyRunWay1 {
     public static void main(String[] args) throws IOException {
-        String[] cows = new String[11];
+        int[] cows = new int[11];
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Arrays.fill(cows, -1);
         int cnt = Integer.parseInt(br.readLine());
-        for (int i =0; i<cnt; i++){
+        int answer = 0;
+        for (int i = 0; i < cnt; i++) {
             StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
             int cowNum = Integer.parseInt(stringTokenizer.nextToken());
-            int location = Integer.parseInt(stringTokenizer.nextToken());
+            int way = Integer.parseInt(stringTokenizer.nextToken());
+            if (cows[cowNum] == -1) {
+                cows[cowNum] = way;
+            } else {
+                if (cows[cowNum] != way) {
+                    answer++;
+                    cows[cowNum] = way;
+                }
+            }
         }
-
+        System.out.println(answer);
     }
+
 }

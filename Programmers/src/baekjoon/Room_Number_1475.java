79c72aec46787904f1d868b5a020355ad3fe27a7
dmstjd0214@naver.com <dmstjd0214@naver.com>
2023-06-14 17:25:30 +0900

    방찾기 해결 하지만 정답처리가 안됨 반례를 찾아야할듯

--- Programmers/src/baekjoon/Room_Number_1475.java
+++ Programmers/src/baekjoon/Room_Number_1475.java
@@ -7,32 +7,33 @@ public class Room_Number_1475 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
-        int answer = 1;
-        int count = 0;
+        int count;
         int before = 0;
-        int now = 1;
+        int now = 0;
         for (int i = 0; i < input.length(); i++) {
+            count = 0;
             for (int j = 0; j < input.length(); j++) {
-                if (input.charAt(i) == 6 && input.charAt(i) == 9) {
-                    if (input.charAt(i) == input.charAt(j)) count++;
-                    if (count == 3) {
-                        now++;
-                        count = 0;
+                if (input.charAt(i) == '6' || input.charAt(i) == '9') {
+                    if (input.charAt(i) == input.charAt(j)) {
+                        count++;
                     }
                 } else {
                     if (input.charAt(i) == input.charAt(j)) count++;
-                    if (count > 1) {
+                    if (count == 1) {
                         now++;
                         count = 0;
                     }
-                    count =0;
-                }
-                if (now > before) {
-                    before = now;
-                    now = 1;
                 }
             }
+            if (input.charAt(i) == '6' || input.charAt(i) == '9') {
+                now = (count / 2) + (count % 2);
+            }
+            if (now >= before) {
+                before = now;
+                now = 0;
+            }
+
         }
-        System.out.println("before = " + before);
+        System.out.println(before);
     }
 }

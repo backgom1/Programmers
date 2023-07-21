a022c927039083d959beb4ad1c5d7040f6892dfa
dmstjd0214@naver.com <dmstjd0214@naver.com>
2023-07-21 17:31:02 +0900

    로또 해결 다트 아직 미해결

--- Programmers/src/Programmers/level1/DartGame.java
+++ Programmers/src/Programmers/level1/DartGame.java
@@ -23,40 +23,46 @@ public class DartGame {
 
 
         // 정규표현식을 사용하여 문자열을 분리
-        Pattern pattern = Pattern.compile("(\\d+[a-zA-Z#]*\\*?)");
+        Pattern pattern = Pattern.compile("(\\d{1,2})([a-zA-Z#])(\\*?)");
         Matcher matcher = pattern.matcher(dartResult);
 
         List<String> powStr = new ArrayList<>();
         List<Integer> powNum = new ArrayList<>();
         // 분리된 문자열을 출력
         while (matcher.find()) {
-            powStr.add(matcher.group());
-            String[] split = matcher.group().split("");
-            double pow = Math.pow(Double.parseDouble(split[0]), dartPow.get(split[1]));
-            if (split.length == 3) {
-                if (split[2].equals("#")) {
-                    pow = pow * -1;
-                }
-            }
-            powNum.add((int) pow);
+            int number = Integer.parseInt(matcher.group(1));
+            String pow = matcher.group(2);
+            String bonus = matcher.group(3);
+            System.out.println("group = " + number);
+            System.out.println("i = " + pow);
+            System.out.println("bonus = " + bonus);
+//            powStr.add(matcher.group());
+//            String[] split = matcher.group().split("");
+//            double pow = Math.pow(Double.parseDouble(split[0]), dartPow.get(split[1]));
+//            if (split.length == 3) {
+//                if (split[2].equals("#")) {
+//                    pow = pow * -1;
+//                }
+//            }
+//            powNum.add((int) pow);
         }
 
-        for (int i = 0; i < powNum.size(); i++) {
-            if (powStr.get(i).contains("*")) {
-                if (i > 0) {
-                    int current = powNum.get(i) * 2;
-                    int before = powNum.get(i - 1) * 2;
-                    powNum.set(i, current);
-                    powNum.set(i - 1, before);
-                } else {
-                    int current = powNum.get(i) * 2;
-                    powNum.set(i, current);
-                }
-            }
-        }
-        for (Integer integer : powNum) {
-            answer += integer;
-        }
+//        for (int i = 0; i < powNum.size(); i++) {
+//            if (powStr.get(i).contains("*")) {
+//                if (i > 0) {
+//                    int current = powNum.get(i) * 2;
+//                    int before = powNum.get(i - 1) * 2;
+//                    powNum.set(i, current);
+//                    powNum.set(i - 1, before);
+//                } else {
+//                    int current = powNum.get(i) * 2;
+//                    powNum.set(i, current);
+//                }
+//            }
+//        }
+//        for (Integer integer : powNum) {
+//            answer += integer;
+//        }
         return answer;
     }
 

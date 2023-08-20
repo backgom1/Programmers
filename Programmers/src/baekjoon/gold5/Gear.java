7a0158a364371c1fff0571057ebfb3c8bd780be1
ENSUNG\dmstjd <dmstjd0214@naver.com>
2023-08-20 20:53:29 +0900

    회전, 톱니바퀴해결

--- Programmers/src/baekjoon/gold5/Gear.java
+++ Programmers/src/baekjoon/gold5/Gear.java
@@ -1,8 +1,6 @@
 package baekjoon.gold5;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Gear {
 
@@ -10,122 +8,69 @@ public class Gear {
         Scanner sc = new Scanner(System.in);
         ArrayList<LinkedList<Integer>> arrayListOfLinkedLists = new ArrayList<>();
 
-        //1 담는 방법
         for (int i = 0; i < 4; i++) {
             LinkedList<Integer> innerLinkedList = new LinkedList<>();
             String inputGear = sc.nextLine();
 
             for (int k = 0; k < inputGear.length(); k++) {
-                String[] split = inputGear.split("");
-                int parseInt = Integer.parseInt(split[k]);
+                int parseInt = Integer.parseInt(inputGear.charAt(k) + "");
                 innerLinkedList.add(parseInt);
             }
             arrayListOfLinkedLists.add(innerLinkedList);
         }
 
-
-        //2
         int K = sc.nextInt();
 
         for (int i = 0; i < K; i++) {
-            int gearNo = sc.nextInt();
+            int gearNo = sc.nextInt() - 1;
             int rotation = sc.nextInt();
-            int gearNumCnt;
 
-
-            if (rotation == 1) {
-                gearNumCnt = gearNo - 1;
-                while (true) {
-                    if (gearNumCnt <= 0) break;
-                    LinkedList<Integer> currentGear = arrayListOfLinkedLists.get(gearNumCnt); //1
-                    gearNumCnt--;
-                    LinkedList<Integer> beforeGear = arrayListOfLinkedLists.get(gearNumCnt); //0
-                    if (beforeGear.get(2) != currentGear.get(6)) {
-
-                    } else {
-                        break;
-                    }
-                }
-                gearNumCnt = gearNo - 1;
-                while (true) {
-                    if (gearNumCnt >= 3) break;
-                    LinkedList<Integer> currentGear = arrayListOfLinkedLists.get(gearNumCnt);
-                    gearNumCnt++;
-                    LinkedList<Integer> futureGear = arrayListOfLinkedLists.get(gearNumCnt);
-                    if (futureGear.get(6) != currentGear.get(2)) {
-                        Integer spin = currentGear.removeLast();
-                        currentGear.addFirst(spin);
-                        if (gearNumCnt == 0) {
-                            Integer lastSpin = currentGear.removeLast();
-                            currentGear.addFirst(lastSpin);
-                            Integer beforeSpin = futureGear.removeFirst();
-                            futureGear.addLast(beforeSpin);
-                        }
-                    } else {
-                        if (gearNumCnt == 0) {
-                            Integer lastSpin = currentGear.removeLast();
-                            currentGear.addFirst(lastSpin);
-                        }
-                        break;
-                    }
-                }
-            } else {
-
-                gearNumCnt = gearNo - 1;
-                while (true) {
-                    if (gearNumCnt <= 0) break;
-                    LinkedList<Integer> currentGear = arrayListOfLinkedLists.get(gearNumCnt);
-                    gearNumCnt--;
-                    LinkedList<Integer> beforeGear = arrayListOfLinkedLists.get(gearNumCnt);
-                    if (beforeGear.get(2) != currentGear.get(6)) {
-                        if (gearNumCnt == 0) {
-                            Integer beforeSpin = beforeGear.removeLast();
-                            beforeGear.addFirst(beforeSpin);
-                            Integer spin = currentGear.removeFirst();
-                            currentGear.addLast(spin);
-                        }
-                    } else {
-                        if (gearNumCnt == 0) {
-                            Integer spin = currentGear.removeFirst();
-                            currentGear.addLast(spin);
-                        }
-                        break;
-                    }
-                }
-                gearNumCnt = gearNo - 1;
-                while (true) {
-                    if (gearNumCnt >= 3) break;
-                    LinkedList<Integer> currentGear = arrayListOfLinkedLists.get(gearNumCnt);
-                    gearNumCnt++;
-                    LinkedList<Integer> futureGear = arrayListOfLinkedLists.get(gearNumCnt);
-                    if (futureGear.get(6) != currentGear.get(2)) {
-                        if (gearNumCnt == 0) {
-                            Integer beforeSpin = futureGear.removeLast();
-                            futureGear.addFirst(beforeSpin);
-                            Integer spin = currentGear.removeFirst();
-                            currentGear.addLast(spin);
-                        }
-                    } else {
-                        if (gearNumCnt == 0) {
-                            Integer spin = currentGear.removeFirst();
-                            currentGear.addLast(spin);
-                        }
-                        break;
-                    }
-                }
-            }
-
-
+            rotateGears(arrayListOfLinkedLists, gearNo, rotation);
         }
 
-        //첫번째 톱니 더하는 메서드
         System.out.println(topGearToothSum(arrayListOfLinkedLists));
+    }
+
+    public static void rotateGears(ArrayList<LinkedList<Integer>> arrayListOfLinkedLists, int gearNo, int rotation) {
+        int[] rotationArray = new int[4];
+        rotationArray[gearNo] = rotation;
+
+        for (int j = gearNo; j > 0; j--) {
+            LinkedList<Integer> currentGear = arrayListOfLinkedLists.get(j);
+            LinkedList<Integer> beforeGear = arrayListOfLinkedLists.get(j - 1);
+            if (beforeGear.get(2) != currentGear.get(6)) {
+                rotationArray[j - 1] = -rotationArray[j];
+            } else {
+                break;
+            }
+        }
+
+
+        for (int j = gearNo; j < 3; j++) {
+            LinkedList<Integer> currentGear = arrayListOfLinkedLists.get(j);
+            LinkedList<Integer> futureGear = arrayListOfLinkedLists.get(j + 1);
+            if (futureGear.get(6) != currentGear.get(2)) {
+                rotationArray[j + 1] = -rotationArray[j];
+            } else {
+                break;
+            }
+        }
+
+
+        for (int j = 0; j < 4; j++) {
+            int rotationType = rotationArray[j];
+            LinkedList<Integer> currentGear = arrayListOfLinkedLists.get(j);
+            if (rotationType == 1) {
+                currentGear.addFirst(currentGear.removeLast());
+            } else if (rotationType == -1) {
+                currentGear.addLast(currentGear.removeFirst());
+            }
+        }
     }
 
     public static int topGearToothSum(ArrayList<LinkedList<Integer>> arrayListOfLinkedLists) {
         int answer = 0;
         for (int i = 0; i < arrayListOfLinkedLists.size(); i++) {
-
             LinkedList<Integer> linkedList = arrayListOfLinkedLists.get(i);
             Integer topGearTooth = linkedList.get(0);
             if (topGearTooth == 1) {
--- Programmers/src/baekjoon/sliver1/ArrayRotation.java
+++ Programmers/src/baekjoon/sliver1/ArrayRotation.java
@@ -16,41 +16,10 @@ public class ArrayRotation {
             }
         }
 
-        //반복을 하기 위한 값 설정
+        //회전
+        rotation(N, M, R, arrayMap);
 
-
-        for (int i = 0; i < R; i++) {
-            //초기화 작업
-            int verticalMinNumber = 0, horizonMinNumber = 0;
-            int verticalMaxNumber = M - 1, verticalLineCnt = M;
-            int horizonMaxNumber = N - 1, horizonLineCnt = N;
-            //반복 시작
-            while (verticalLineCnt >= 0 || horizonLineCnt >= 0) {
-
-                for (int j = verticalLineCnt - 1; j < 0; j++) {
-                    arrayMap[j][verticalMinNumber] = arrayMap[j - 1][verticalMinNumber];
-                }
-
-                for (int j = horizonLineCnt - 1; j < 0; j++) {
-                    arrayMap[horizonMinNumber][j] = arrayMap[horizonMinNumber][j - 1];
-                }
-
-                for (int j = verticalLineCnt-1; j > 0; j++) {
-                    arrayMap[j][verticalMaxNumber] = arrayMap[j + 1][verticalMaxNumber];
-                }
-
-                for (int j = horizonLineCnt-1; j < 0; j++) {
-                    arrayMap[horizonMaxNumber][j] = arrayMap[horizonMaxNumber][j+1];
-                }
-
-
-                verticalMinNumber++;
-                horizonMinNumber++;
-                verticalMaxNumber--;
-                horizonMaxNumber--;
-            }
-
-        }
+        //함수 나타내기
         for (int[] ints : arrayMap) {
             System.out.println();
             for (int anInt : ints) {
@@ -58,5 +27,33 @@ public class ArrayRotation {
             }
         }
 
+    }
+
+    private static void rotation(int N, int M, int R, int[][] arrayMap) {
+        for (int i = 0; i < R; i++) {
+
+            for (int j = 0; j < Math.min(N, M) / 2; j++) {
+
+                int shiftNumber = arrayMap[j][j];
+
+                for (int k = j; k < M - j - 1; k++) {
+                    arrayMap[j][k] = arrayMap[j][k + 1];
+                }
+
+                for (int k = j; k < N - j - 1; k++) {
+                    arrayMap[k][M - j - 1] = arrayMap[k + 1][M - j - 1];
+                }
+
+                for (int k = M - j - 1; k > j; k--) {
+                    arrayMap[N - j - 1][k] = arrayMap[N - j - 1][k -1];
+                }
+
+                for (int k = N - j - 1; k > j; k--) {
+                    arrayMap[k][j] = arrayMap[k - 1][j];
+                }
+
+                arrayMap[j + 1][j] = shiftNumber;
+            }
+        }
     }
 }

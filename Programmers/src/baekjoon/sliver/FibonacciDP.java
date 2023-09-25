5008525581fad31144e5ee6cbe369a18dc88ec57
dmstjd0214@naver.com <dmstjd0214@naver.com>
2023-09-25 17:19:22 +0900

    피보나치 해결

--- Programmers/src/baekjoon/sliver/FibonacciDP.java
+++ Programmers/src/baekjoon/sliver/FibonacciDP.java
@@ -1,54 +1,33 @@
 package baekjoon.sliver;
 
-import com.sun.tools.jconsole.JConsoleContext;
-
-import java.util.Scanner;
+import java.io.*;
 
 public class FibonacciDP {
+    static int[] zeroDp = new int[41];
+    static int[] oneDp = new int[41];
 
-    public static int[] zeroCnt = new int[41];
-    public static int[] oneCnt = new int[41];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+        int T = Integer.parseInt(br.readLine());
 
-        //0이 있는 케이스가 있을니까 -1로 초기화함
-        for (int i = 0; i < 41; i++) {
-            zeroCnt[i] = -1;
-            oneCnt[i] = -1;
-        }
-        zeroCnt[0] = 1;
-        zeroCnt[1] = 0;
-        oneCnt[0] = 0;
-        oneCnt[1] = 1;
+        zeroDp[0] = 1;
+        zeroDp[1] = 0;
+        oneDp[0] = 0;
+        oneDp[1] = 1;
 
         for (int i = 0; i < T; i++) {
-
-            int targetNumber = sc.nextInt();
-            int zeroCnt = Getcnt(targetNumber, false);
-            int oneCnt = Getcnt(targetNumber, true);
-            System.out.println(zeroCnt + " " + oneCnt);
+            int targetNum = Integer.parseInt(br.readLine());
+            for (int j = 2; j <= targetNum; j++) {
+                if (zeroDp[j] == 0) zeroDp[j] = zeroDp[j - 1] + zeroDp[j - 2];
+                if (oneDp[j] == 0) oneDp[j] = oneDp[j - 1] + oneDp[j - 2];
+            }
+            bw.write(zeroDp[targetNum] + " " + oneDp[targetNum]);
+            bw.write("\n");
         }
+        bw.flush();
+        bw.close();
     }
 
-    public static int Getcnt(int num, boolean type) {
-
-        //아직 값이 준비가 되지 않았다
-        if (zeroCnt[num] == -1 && oneCnt[num] == -1) {
-            zeroCnt[num] = Getcnt(num - 1, false) + Getcnt(num - 2, false);
-            oneCnt[num] = Getcnt(num - 1, true) + Getcnt(num - 2, true);
-            if (type) {
-                return oneCnt[num];
-            } else {
-                return zeroCnt[num];
-            }
-        } else {
-            if (type) {
-                return oneCnt[num];
-            } else {
-                return zeroCnt[num];
-            }
-        }
-    }
 }
